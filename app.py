273adc85f0da7352fcff4febe7e0b097df83bb22
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-17 08:37:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import pandas as pd
 import numpy as np
 
 st.title("PRU Feedback Survey Dashboard")
+
+col1, col2 = st.beta_columns([3,1])
+
+data = pd.read_csv('sample.csv')
+
+# Extract the date of responses
+dates = data[['Timestamp','Which Year of Study are you currently in?']].groupby(['Timestamp']).count()
+
+col1.subheader("Response Timeline")
+col1.line_chart(dates)
