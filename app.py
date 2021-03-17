e3a2d38eb0bd50d6e45d1ff12fd81a454a29af2f
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-17 08:42:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ col1, col2 = st.beta_columns([3,1])
 data = pd.read_csv('sample.csv')
 
 # Extract the date of responses
+data.Timestamp = data.Timestamp.apply(lambda x:pd.to_datetime(x[:10]))
 dates = data[['Timestamp','Which Year of Study are you currently in?']].groupby(['Timestamp']).count()
 
 col1.subheader("Response Timeline")
