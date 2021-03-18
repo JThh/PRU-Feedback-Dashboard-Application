80faa877711e5ec21e9889efae69ccd8e7bbf589
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-18 23:16:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,25 +37,55 @@ with st.beta_expander("Demography Analysis"):
   col1,col2 = st.beta_columns(2)
 
   col1.subheader('Distribution of Respondent Home Faculty')
-  df_faculty = data['Which Faculty are you from? (Indicate your home faculty if you are in a double-degree programme)'].value_counts()
-  df_year = data['Which Year of Study are you currently in?'].value_counts()
   
-  fig, ax = plt.subplots()
-  labels = df_faculty.index
-  plt.pie(x=df_faculty, 
-          autopct="%.1f%%", 
-          explode=[0.05]*df_faculty.index.shape[0], 
-          labels=labels, 
-          pctdistance=0.5)
- 
-  plt.title("Distribution of Home Faculty", fontsize=14)
-  col1.pyplot(fig)
+  data['count'] = 1
+  
+  data.rename({'Which Faculty are you from? (Indicate your home faculty if you are in a double-degree programme)':'Faculty',
+              'Which Year of Study are you currently in?': 'Year of Study'}, axis='columns')
+  df_faculty = data[['Faculty','count']].groupby(['Faculty']).count()
+  df_year = data[['Year of Study','count']].groupby(['Year of Study']).count()
+  
+  fig = px.pie(
+      df_faculty, 
+      names='Faculty', 
+      values="count", 
+      color='Faculty',
+      title='Which Faculty are you from?', 
+  )
+  col1.plotly_chart(fig)
   
   col2.subheader('Distribution of Respondent Year of Study')
-  fig, ax = plt.subplots()
-  plt.bar(df_year.index,df_year.values,width=0.3)
-  plt.title("Distribution of Year of Study", fontsize=14)
-  col2.pyplot(fig)
+  fig = px.bar(
+      df_year, 
+      x="count", 
+      y="Year of Study", 
+      color = "Year of Study",
+      orientation='h', 
+      title='Which year of study are you currently in', 
+  )
+  
+  col2.plotly_chart(fig)  
+  
+  
+  
+  
+  
+#   fig, ax = plt.subplots()
+#   labels = df_faculty.index
+#   plt.pie(x=df_faculty, 
+#           autopct="%.1f%%", 
+#           explode=[0.05]*df_faculty.index.shape[0], 
+#           labels=labels, 
+#           pctdistance=0.5)
+ 
+#   plt.title("Distribution of Home Faculty", fontsize=14)
+#   col1.pyplot(fig)
+  
+#   col2.subheader('Distribution of Respondent Year of Study')
+#   fig, ax = plt.subplots()
+#   plt.bar(df_year.index,df_year.values,width=0.3)
+#   plt.title("Distribution of Year of Study", fontsize=14)
+#   col2.pyplot(fig)
 
 
 # with st.beta_expander("Textual Analysis"):
