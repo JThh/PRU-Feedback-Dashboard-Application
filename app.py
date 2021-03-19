9cc5dbd4b1faca1d40579b532f9597b13949241d
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-19 09:12:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,24 +17,24 @@ data = pd.read_csv('sample.csv')
 
 # Number of responses along the timeline
 
-data.Timestamp = data.Timestamp.apply(lambda x:pd.to_datetime(x[:10]))
-dates = data.Timestamp.value_counts().sort_index()
+with st.beta_expander('Time Series Analysis'):
+  data.Timestamp = data.Timestamp.apply(lambda x:pd.to_datetime(x[:10]))
+  dates = data.Timestamp.value_counts().sort_index()
 
-timeindex = pd.date_range(start=dates.index[0],end=dates.index[-1])
-all_dates = pd.DataFrame(columns=['Number_of_replies'],index=timeindex)
+  timeindex = pd.date_range(start=dates.index[0],end=dates.index[-1])
+  all_dates = pd.DataFrame(columns=['Number_of_replies'],index=timeindex)
 
-for i in timeindex:
-  if i in dates.index:
-    all_dates.loc[i,'Number_of_replies'] = dates[i]
-  else:
-    all_dates.loc[i,'Number_of_replies'] = 0
+  for i in timeindex:
+    if i in dates.index:
+      all_dates.loc[i,'Number_of_replies'] = dates[i]
+    else:
+      all_dates.loc[i,'Number_of_replies'] = 0
 
-st.subheader("Response Timeline")
-st.line_chart(all_dates)
+  st.subheader("Response Timeline")
+  st.line_chart(all_dates)
 
 with st.beta_expander("Demography Analysis"):
   
-
   st.subheader('Distribution of Respondent Home Faculty')
   
   data['count'] = 1
@@ -65,9 +65,39 @@ with st.beta_expander("Demography Analysis"):
   
   st.plotly_chart(fig)  
   
+With st.beta_expander('Textual Analysis'):
   
+  df_zoning = data['I feel that the zoning restrictions were ________.'].dropna()
+
+  st.header('I feel that the zoning restrictions were ________.')
+  if st.checkbox('Show n random comments',True,key='1'):
+    number = st.slider('Number of comments to take a look at:', 1, 10, 5,key='1')
+    sample = df_zoning.sample(number)
+    st.table(sample)
   
+  st.subheader('Wordcloud')
   
+  def build_wordcloud(df, title):
+    wordcloud = WordCloud(
+        background_color='gray', 
+        stopwords=set(STOPWORDS), 
+        max_words=50, 
+        max_font_size=40, 
+        random_state=666
+    ).generate(str(df))
+
+    fig = plt.figure(1, figsize=(14,14))
+    plt.axis('off')
+    fig.suptitle(title, fontsize=16)
+    fig.subplots_adjust(top=2.3)
+
+    plt.imshow(wordcloud)
+    st.pyplot()
+   
+  clouds = st.slider('Select n comments to visualize their collective word clouds',1,df_zoning.shape[0],key='select')
+  if st.button('Click to show the cloud',key='cloud'):
+      build_wordcloud(df_zoning.sample(clouds), f'Word Cloud for {clouds} sampled comments')
+
   
 #   fig, ax = plt.subplots()
 #   labels = df_faculty.index
