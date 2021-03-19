387a54f56b163a79833d920129bd33caf86e94ef
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-19 10:43:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,15 +143,15 @@ with st.beta_expander('Textual Analysis'):
   combined_with = st.multiselect('Combine the result with', ('Faculty','Year of Study'),key='combo')
   
   if 'Faculty' in combined_with:
-    faculties = df_faculty.Faculty.unique()
+    faculties = df_faculty.Faculty
     df_fac_sen = pd.concat([df_zoning_score.category, data.Faculty],axis=1)
     df_fac_sen['count'] = 1
     fac_pos = df_fac_sen.groupby(['category','Faculty']).count()
    
     fig = go.Figure(data=[
-        go.Bar(name='Negative', x=faculties, y=fac_pos.loc['Negative'].values),
-        go.Bar(name='Neutral', x=faculties, y=fac_pos.loc['Neutral'].values),
-        go.Bar(name='Positive', x=faculties, y=fac_pos.loc['Positive'].values)
+        go.Bar(name='Negative', x=faculties, y=fac_pos.loc['Negative']),
+        go.Bar(name='Neutral', x=faculties, y=fac_pos.loc['Neutral']),
+        go.Bar(name='Positive', x=faculties, y=fac_pos.loc['Positive'])
     ])
     # Change the bar mode
     fig.update_layout(barmode='group')
