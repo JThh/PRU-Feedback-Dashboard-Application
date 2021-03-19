e60d90079e4d1f194f66ad439459d53e4e860ae1
JT.Han <59948448+Jiatong-han@users.noreply.github.com>
2021-03-19 10:03:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 import plotly.express as px
 from plotly.subplots import make_subplots
 from plotly.offline import init_notebook_mode, iplot
@@ -9,9 +10,40 @@ import plotly.graph_objects as go
 import plotly.graph_objs as go
 from wordcloud import WordCloud, STOPWORDS
 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 # Disable the warnings.
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+# Some utility functions.
+def build_wordcloud(df, title):
+  wordcloud = WordCloud(
+      background_color='gray', 
+      stopwords=set(STOPWORDS), 
+      max_words=50, 
+      max_font_size=40, 
+      random_state=666
+  ).generate(str(df))
+
+  fig = plt.figure(1, figsize=(14,14))
+  plt.axis('off')
+  fig.suptitle(title, fontsize=16)
+  fig.subplots_adjust(top=2.3)
+
+  plt.imshow(wordcloud)
+  st.pyplot()
+  
+def categorize(senti):
+  if senti < 0:
+    return 'Negative'
+  elif senti == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
+  
+  
+ 
+# Set the title and basic layout of the application.
 st.title("PRU Feedback Survey Dashboard")
 st.markdown('Exploratory Analysis on Some Specific Questions')
 
@@ -34,9 +66,10 @@ with st.beta_expander('Time Series Analysis'):
       all_dates.loc[i,'Number_of_replies'] = 0
 
   st.subheader("Response Timeline")
-  st.area_chart(all_dates)
   
   st.dataframe(all_dates)
+  st.line_chart(all_dates)
+  
 
 with st.beta_expander("Demography Analysis"):
   
@@ -72,57 +105,54 @@ with st.beta_expander("Demography Analysis"):
   
 with st.beta_expander('Textual Analysis'):
   
-  df_zoning = data['I feel that the zoning restrictions were ________.'].dropna()
+  df_zoning = data['I feel that the zoning restrictions were ________.'].rename({'I feel that the zoning restrictions were ________.':'comments'},axis=1)
 
-  st.header('I feel that the zoning restrictions were ________.')
+  st.header('I feel that the zoning restrictions were _.')
   if st.checkbox('Show n random comments',True,key='1'):
     number = st.slider('Number of comments to take a look at:', 1, 10, 5,key='1')
     sample = df_zoning.sample(number)
     st.table(sample)
   
   st.subheader('Wordcloud')
-  
-  def build_wordcloud(df, title):
-    wordcloud = WordCloud(
-        background_color='gray', 
-        stopwords=set(STOPWORDS), 
-        max_words=50, 
-        max_font_size=40, 
-        random_state=666
-    ).generate(str(df))
-
-    fig = plt.figure(1, figsize=(14,14))
-    plt.axis('off')
-    fig.suptitle(title, fontsize=16)
-    fig.subplots_adjust(top=2.3)
-
-    plt.imshow(wordcloud)
-    st.pyplot()
+ 
    
   clouds = st.slider('Select n comments to visualize their collective word clouds',1,df_zoning.shape[0],key='select')
   if st.button('Click to show the cloud',key='cloud'):
       build_wordcloud(df_zoning.sample(clouds), f'Word Cloud for {clouds} sampled comments')
+      
+    
+  
+  st.subheader('Sentiment Analysis')
+  st.markdown('The sentiment is evaluated as a polarity score ranging from -1 to 1. The lower the comment is scored, the less positive it tends to be.')
+  
+  sid = SentimentIntensityAnalyzer()
+  scores = pd.Series([sid.polarity_score(text) for text in df_zoning.comments],name='score')
+  df_zoning_score = pd.concat([df_zoning, scores],axis=1)
+  if st.checkbox('Show some examples',False,key='score'):
+    st.table(df_zoning_score.sample(5))
+    
+  df_zoning_score['category'] = df_zoning_score.scores.apply(lambda x:categorize(x))
+    
+  st.markdown('To make easier comparison, the sentiment is categoried into \'positive\', \'neutral\' and \'negative\' based on their polarity score. Now it\'s time to visualize our findings.')
+  
+  combined_with = st.multiselect('Combine the result with', ('Faculty','Year of Study'),key='combo')
+  
+  if 'Faculty' in combined_with:
+    faculties = df_faculty.Faculty.unique()
+    df_fac_sen = pd.concat([df_zoning_score.category, df_faculty.Faculty],axis=1)
+    fac_pos = df_fac_sen.groupby(['category','Faculty']).count()
+    fig = go.Figure(data=[
+        go.Bar(name='Negative', x=faculties, y=fac_pos['Negative'].values),
+        go.Bar(name='Neutral', x=faculties, y=fac_pos['Neutral'].values),
+        go.Bar(name='Positive', x=faculties, y=fac_pos['Positive'].values)
+    ])
+    # Change the bar mode
+    fig.update_layout(barmode='group')
+    fig.show()
 
   
-#   fig, ax = plt.subplots()
-#   labels = df_faculty.index
-#   plt.pie(x=df_faculty, 
-#           autopct="%.1f%%", 
-#           explode=[0.05]*df_faculty.index.shape[0], 
-#           labels=labels, 
-#           pctdistance=0.5)
- 
-#   plt.title("Distribution of Home Faculty", fontsize=14)
-#   col1.pyplot(fig)
-  
-#   col2.subheader('Distribution of Respondent Year of Study')
-#   fig, ax = plt.subplots()
-#   plt.bar(df_year.index,df_year.values,width=0.3)
-#   plt.title("Distribution of Year of Study", fontsize=14)
-#   col2.pyplot(fig)
 
 
-# with st.beta_expander("Textual Analysis"):
   
   
   
